bf653954dbf7fdebb50e34987baf8018e12f1d9d
youyo <1003ni2@gmail.com>
2017-09-29 12:01:50 +0900

    bump up

--- hidy.rb
+++ hidy.rb
@@ -1,8 +1,8 @@
 class Hidy < Formula
   desc "Manage credentials using aws parameter store."
   homepage "https://github.com/youyo/hidy"
-  url "https://github.com/youyo/hidy/releases/download/0.9.0/hidy"
-  sha256 "959e6658140db474e5407830397cb50b5e32e70322561c9d50b537be5ee6867c"
+  url "https://github.com/youyo/hidy/releases/download/0.9.1/hidy"
+  sha256 "d4f3e57cc88d91290e8fd877fe935a696cd50ebfa277b98a3450a841547387f6"
 
   def install
     bin.install "hidy"
